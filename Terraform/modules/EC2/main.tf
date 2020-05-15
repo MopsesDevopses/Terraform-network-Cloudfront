02eb6eb9577e43dea3f78210f522afc366ba458e
MopsesDevopses <63007614+MopsesDevopses@users.noreply.github.com>
2020-05-15 13:55:02 +0300

    Update main.tf

--- Terraform/modules/EC2/main.tf
+++ Terraform/modules/EC2/main.tf
@@ -12,7 +12,7 @@ resource "aws_autoscaling_group" "project" {
   max_size                  = "${var.asg_max_size}"
   min_size                  = "${var.asg_min_size}"
   desired_capacity          = "${var.asg_desired_capacity}"
-  vpc_zone_identifier       = "${var.private_subnet_ids}"
+  vpc_zone_identifier       = "${var.public_subnet_ids}"
   launch_configuration      = aws_launch_configuration.project.name
   health_check_type         = "ELB"
   health_check_grace_period = 600
